1f90339d73074cd2637986c36988dfae03046047
mahteus-sanches <matheus.sanches9@protonmail.com>
2024-03-24 22:30:36 -0300

    fiz o jogo da cobrinha com ajuda do chat gpt, faltam ainda vários detalhes, mas foi o primeiro passo

--- teste_ski_image.py
+++ teste_ski_image.py
@@ -1,2 +1,11 @@
 import skimage as ski
 print(ski.__version__)
+camera = ski.data.camera()
+print(camera.shape)
+print(camera.size)
+print(camera.min())
+print(camera.max())
+print(camera.mean())
+print(camera[10,0])
+camera[10,0] = 0
+print(camera[10,0])
